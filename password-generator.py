315d711254bf9b2cf35e365f1cae4981f6f56fb0
howiely <howardly@student.ubc.ca>
2022-01-21 01:50:17 -0800

    Added comments for readability

--- password-generator.py
+++ password-generator.py
@@ -1,12 +1,18 @@
 import random
+# import random module from python
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!-?,@$'
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!-?,@$_'
+# characters used to generate passwords
 
 pw_amount = input("Enter the amount of passwords you'd like to generate: ")
+# input the amount of passwords to be generated
 pw_amount = int(pw_amount)
+# turns pw_amount from String -> Int
 
 pw_length = input('Enter the desired password length: ')
+# input the length of the generated password(s)
 pw_length = int(pw_length)
+# turns pw_length from String -> Int
 
 for p in range(pw_amount):
     password = ''
